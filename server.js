3afea219150d3f1d55009745c97f01e451252095
Eric Pan <epan@users.noreply.github.com>
2017-06-01 09:35:38 -0700

    (feat) Serve static frontend

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const logger = require('morgan');
+const path = require('path');
 
 app.use(logger('dev'));
+app.use(express.static('./client'));
 
 app.listen(1337, function () {
   console.log('Message bus is listening on 1337');
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
